a8eae00ab79f3848dacc22c8012a503dd97fe8a4
michaellangbein <michael_langbein@hotmail.de>
2020-10-13 07:17:06 +0200

    basic setup openlayers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-import { add } from "./utils/utils";
+import { Map, View } from 'ol';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { GeoJSON } from 'ol/format';
+import 'ol/ol.css';
 
-const sum = add(1, 2);
-console.log(sum);
+
+fetch('./data/buildings.geojson').then((response: Response) => {
+    response.json().then(data => {
+        const dataLayer = new VectorLayer({
+            source: new VectorSource({
+                features: new GeoJSON().readFeatures(data)
+            })
+        });
+
+        map.addLayer(dataLayer);
+    });
+});
+
+
+const anchor = document.getElementById('map') as HTMLDivElement;
+
+
+const bgLayer = new TileLayer({
+    source: new OSM()
+});
+
+const view = new View({
+    projection: 'EPSG:4326',
+    center: [15, 52],
+    zoom: 6
+});
+
+const map = new Map({
+    layers: [bgLayer],
+    target: anchor,
+    view: view
+});
--- src/utils/utils.spec.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./utils";
-
-describe("utils test-suite", () => {
-  it("adding should work", () => {
-    expect(add(1, 2)).toEqual(3);
-  });
-});
