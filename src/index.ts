263e19ed24d77c89a35bc5ee9cf67c286490c0ee
Langbein, Michael <michael.langbein@dlr.de>
2020-12-01 20:52:28 +0100

    voxel basics working

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { AmbientLight, BoxGeometry, BufferAttribute, BufferGeometry, BufferGeometryUtils, DirectionalLight, DoubleSide, Mesh, MeshBasicMaterial, MeshLambertMaterial, MeshPhongMaterial, PerspectiveCamera, Scene, Vector3, VertexColors, WebGLRenderer, WireframeGeometry } from 'three';
+import {
+    AmbientLight, BoxGeometry, BufferAttribute, BufferGeometry, BufferGeometryUtils,
+    Color,
+    DirectionalLight, DoubleSide, Mesh, MeshBasicMaterial, MeshLambertMaterial,
+    MeshPhongMaterial, PerspectiveCamera, PlaneGeometry, Scene, Vector3, VertexColors,
+    WebGLRenderer, WireframeGeometry
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { createAttributes, createBlockMesh } from './utils/utils';
 
 
 const container = document.getElementById('map') as HTMLCanvasElement;
+const slider = document.getElementById('rangeSlider') as HTMLInputElement;
 
 const scene = new Scene();
 const renderer = new WebGLRenderer({
     canvas: container
 });
 
-const camera = new PerspectiveCamera(75, container.width/container.height, 0.1, 1000);
-camera.position.z = 5;
+const camera = new PerspectiveCamera(75, container.width / container.height, 0.1, 1000);
+camera.position.z = 10;
+camera.position.y = 4;
 
 const controls = new OrbitControls(camera, container);
 
@@ -23,98 +32,26 @@ scene.add(light);
 const light2 = new AmbientLight('yellow', 0.5);
 scene.add(light2);
 
-const geometry = new BoxGeometry();
-const material = new MeshPhongMaterial( { color: 0x00ff00 } );
-const cube = new Mesh( geometry, material );
-scene.add( cube );
+const planeGeom = new PlaneGeometry(80, 80);
+const planeMaterial = new MeshBasicMaterial({ color: new Color('rgb(0, 100, 225)'), side: DoubleSide, transparent: true, opacity: 0.5 });
+const cutPlane = new Mesh(planeGeom, planeMaterial);
+cutPlane.position.setX(-20);
+cutPlane.lookAt(-1, 0, 0);
+scene.add(cutPlane);
 
 
-/**
- * Polygon model: hardcoded model
- * Voxel model: data-driven models
- */
 
-// cube data
-const cubeData = {
-    'left': {
-        normal: [-1, 0, 0],
-        vertices: [
-            [-1,  1,  1],
-            [-1,  1, -1],
-            [-1, -1, -1],
-            [-1,  1,  1],
-            [-1, -1, -1],
-            [-1, -1,  1]
-        ],
-    },
-    'right': {
-        normal: [1, 0, 0],
-        vertices: [
-            [ 1,  1,  1],
-            [ 1, -1, -1],
-            [ 1,  1, -1],
-            [ 1,  1,  1],
-            [ 1, -1,  1],
-            [ 1, -1, -1],
-        ],
-    },
-    'top': {
-        normal: [0, 1, 0],
-        vertices: [
-            [-1,  1,  1],
-            [ 1,  1,  1],
-            [ 1,  1, -1],
-            [-1,  1,  1],
-            [ 1,  1, -1],
-            [-1,  1, -1],
-        ],
-    },
-    'bottom': {
-        normal: [0, -1, 0],
-        vertices: [
-            [-1, -1,  1],
-            [ 1, -1, -1],
-            [ 1, -1,  1],
-            [-1, -1,  1],
-            [-1, -1, -1],
-            [ 1, -1, -1],
-        ],
-    },
-    'front': {
-        normal: [0, 0, 1],
-        vertices: [
-            [-1,  1, 1],
-            [-1, -1, 1],
-            [ 1,  1, 1],
-            [ 1,  1, 1],
-            [-1, -1, 1],
-            [ 1, -1, 1],
-        ],
-    },
-    'back': {
-        normal: [0, 0, -1],
-        vertices: [
-            [-1,  1, -1],
-            [ 1,  1, -1],
-            [-1, -1, -1],
-            [ 1,  1, -1],
-            [ 1, -1, -1],
-            [-1, -1, -1],
-        ],
-    },
-};
-
-const colorFunc = (val: number) => {
-    switch(val) {
+const colorFunc = (val: number): [number, number, number] => {
+    switch (val) {
         case 0:
             return [0, 0, 0];
         case 1:
             return [1, 0, 0];
         case 2:
             return [0, 1, 0];
-        case 3: 
+        case 3:
             return [0, 0, 1];
-        case 4: 
+        case 4:
             return [1, 1, 0];
         case 5:
             return [1, 0, 1];
@@ -122,115 +59,64 @@ const colorFunc = (val: number) => {
             return [0, 1, 1];
         case 7:
             return [1, 1, 1];
-        default: 
+        default:
             return [0, 0, 0];
     }
 };
 
+const X = 80;
+const Y = 30;
+const Z = 80;
 
-// Voxel data
-const X = 25;
-const Y = 25;
-const Z = 25;
-const cubeSize = 0.25;
-
-const data: number[][][] = [];
+const allData: number[][][] = [];
 for (let x = 0; x < X; x++) {
-    data.push([]);
+    allData.push([]);
     for (let y = 0; y < Y; y++) {
-        data[x].push([]);
+        allData[x].push([]);
         for (let z = 0; z < Z; z++) {
-            if (y < 10 * Math.sin(x * 0.1) * Math.cos(z * 0.1)) {
-                data[x][y].push(1 + Math.floor(Math.random() * 4));
+            if (y < 10 * Math.sin(x * 0.1) * Math.cos(z * 0.1) + 5) {
+                allData[x][y].push(1 + Math.floor(Math.random() * 4));
             } else {
-                data[x][y].push(0);
+                allData[x][y].push(0);
             }
         }
     }
 }
 
-const vertices: number[][] = [];
-const normals: number[][] = [];
-const colors: number[][] = [];
-for (let x = 0; x < X; x++) {
-    for (let y = 0; y < Y; y++) {
-        for (let z = 0; z < Z; z++) {
-            if (data[x][y][z] !== 0) {
-
-                // left neighbor
-                if (x === 0 || data[x-1][y][z] === 0) {
-                    for (const vertex of cubeData.left.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.left.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-                // right neighbor
-                if (x === X-1 || data[x+1][y][z] === 0) {
-                    for (const vertex of cubeData.right.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.right.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-                // top neighbor
-                if (y === Y-1 || data[x][y+1][z] === 0) {
-                    for (const vertex of cubeData.top.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.top.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-                // bottom neighbor
-                if (y === 0 || data[x][y-1][z] === 0) {
-                    for (const vertex of cubeData.bottom.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.bottom.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-                // front neighbor
-                if (z === Z-1 || data[x][y][z+1] === 0) {
-                    for (const vertex of cubeData.front.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.front.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-                // back neighbor
-                if (z === 0 || data[x][y][z-1] === 0) {
-                    for (const vertex of cubeData.back.vertices) {
-                        vertices.push([cubeSize * (vertex[0]/2 + x), cubeSize * (vertex[1]/2 + y), cubeSize * (vertex[2]/2 + z)]);
-                        normals.push(cubeData.back.normal);
-                        colors.push(colorFunc(data[x][y][z]));
-                    }
-                }
-                
-            }
-        }
-    }
-}
-
-const voxelBlockGeometry = new BufferGeometry();
-voxelBlockGeometry.setAttribute('position', new BufferAttribute(new Float32Array(vertices.flat()), 3));
-voxelBlockGeometry.setAttribute('normal', new BufferAttribute(new Float32Array(normals.flat()), 3));
-voxelBlockGeometry.setAttribute('color', new BufferAttribute(new Float32Array(colors.flat()), 3));
-const voxelBlockMaterial = new MeshLambertMaterial({ vertexColors: true, side: DoubleSide, wireframe: false });
-const voxelBlock = new Mesh(voxelBlockGeometry, voxelBlockMaterial);
+const blockSize = 0.5;
+const translation: [number, number, number] = [-20, -10, 0];
+const voxelBlock = createBlockMesh(allData, blockSize, [X, Y, Z], translation, colorFunc);
 scene.add(voxelBlock);
 
+slider.addEventListener('input', (ev: Event) => {
+    const val = +(slider.value);
+    cutPlane.position.setX(val);
 
+    const newData: number[][][] = [];
+    for (let x = 0; x < X; x++) {
+        newData.push([]);
+        for (let y = 0; y < Y; y++) {
+            newData[x].push([]);
+            for (let z = 0; z < Z; z++) {
+                const xVal = translation[0] + x * blockSize / 2;
+                if (xVal < val) {
+                    newData[x][y][z] = 0;
+                } else {
+                    newData[x][y][z] = allData[x][y][z];
+                }
+            }
+        }
+    }
+
+    const newAttrs = createAttributes(newData, blockSize, [X, Y, Z], translation, colorFunc);
+    (voxelBlock.geometry as BufferGeometry).setAttribute('position', newAttrs.position);
+    (voxelBlock.geometry as BufferGeometry).setAttribute('normal', newAttrs.normal);
+    (voxelBlock.geometry as BufferGeometry).setAttribute('color', newAttrs.color);
+});
 
 
 function animate() {
-    requestAnimationFrame( animate );
-    cube.rotateX(0.01);
-    cube.rotateY(0.01);
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 animate();
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,188 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { BufferAttribute, BufferGeometry, DoubleSide, Mesh, MeshLambertMaterial } from 'three';
+
+
+
+const cubeData = {
+  'left': {
+      normal: [-1, 0, 0],
+      vertices: [
+          [-1, 1, 1],
+          [-1, 1, -1],
+          [-1, -1, -1],
+          [-1, 1, 1],
+          [-1, -1, -1],
+          [-1, -1, 1]
+      ],
+  },
+  'right': {
+      normal: [1, 0, 0],
+      vertices: [
+          [1, 1, 1],
+          [1, -1, -1],
+          [1, 1, -1],
+          [1, 1, 1],
+          [1, -1, 1],
+          [1, -1, -1],
+      ],
+  },
+  'top': {
+      normal: [0, 1, 0],
+      vertices: [
+          [-1, 1, 1],
+          [1, 1, 1],
+          [1, 1, -1],
+          [-1, 1, 1],
+          [1, 1, -1],
+          [-1, 1, -1],
+      ],
+  },
+  'bottom': {
+      normal: [0, -1, 0],
+      vertices: [
+          [-1, -1, 1],
+          [1, -1, -1],
+          [1, -1, 1],
+          [-1, -1, 1],
+          [-1, -1, -1],
+          [1, -1, -1],
+      ],
+  },
+  'front': {
+      normal: [0, 0, 1],
+      vertices: [
+          [-1, 1, 1],
+          [-1, -1, 1],
+          [1, 1, 1],
+          [1, 1, 1],
+          [-1, -1, 1],
+          [1, -1, 1],
+      ],
+  },
+  'back': {
+      normal: [0, 0, -1],
+      vertices: [
+          [-1, 1, -1],
+          [1, 1, -1],
+          [-1, -1, -1],
+          [1, 1, -1],
+          [1, -1, -1],
+          [-1, -1, -1],
+      ],
+  },
+};
+
+
+
+export function createBlockMesh(data: number[][][], cubeSize: number, blockDimensions: [number, number, number], translation: [number, number, number], colorFunc: (n: number) => [number, number, number]): Mesh {
+  const attrs = createAttributes(data, cubeSize, blockDimensions, translation, colorFunc);
+
+  const voxelBlockGeometry = new BufferGeometry();
+  voxelBlockGeometry.setAttribute('position', attrs.position);
+  voxelBlockGeometry.setAttribute('normal', attrs.normal);
+  voxelBlockGeometry.setAttribute('color', attrs.color);
+  const voxelBlockMaterial = new MeshLambertMaterial({ vertexColors: true, side: DoubleSide, wireframe: false });
+  const voxelBlock = new Mesh(voxelBlockGeometry, voxelBlockMaterial);
+
+  return voxelBlock;
+}
+
+export function createAttributes(data: number[][][], cubeSize: number, blockDimensions: [number, number, number], translation: [number, number, number], colorFunc: (n: number) => [number, number, number]) {
+  const [X, Y, Z] = blockDimensions;
+
+  const vertices: number[][] = [];
+  const normals: number[][] = [];
+  const colors: number[][] = [];
+  for (let x = 0; x < X; x++) {
+      for (let y = 0; y < Y; y++) {
+          for (let z = 0; z < Z; z++) {
+              if (data[x][y][z] !== 0) {
+
+                  // left neighbor
+                  if (x === 0 || data[x - 1][y][z] === 0) {
+                      for (const vertex of cubeData.left.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.left.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+                  // right neighbor
+                  if (x === X - 1 || data[x + 1][y][z] === 0) {
+                      for (const vertex of cubeData.right.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.right.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+                  // top neighbor
+                  if (y === Y - 1 || data[x][y + 1][z] === 0) {
+                      for (const vertex of cubeData.top.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.top.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+                  // bottom neighbor
+                  if (y === 0 || data[x][y - 1][z] === 0) {
+                      for (const vertex of cubeData.bottom.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.bottom.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+                  // front neighbor
+                  if (z === Z - 1 || data[x][y][z + 1] === 0) {
+                      for (const vertex of cubeData.front.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.front.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+                  // back neighbor
+                  if (z === 0 || data[x][y][z - 1] === 0) {
+                      for (const vertex of cubeData.back.vertices) {
+                          vertices.push([
+                              cubeSize * (vertex[0] / 2 + x) + translation[0],
+                              cubeSize * (vertex[1] / 2 + y) + translation[1],
+                              cubeSize * (vertex[2] / 2 + z) + translation[1]
+                          ]);
+                          normals.push(cubeData.back.normal);
+                          colors.push(colorFunc(data[x][y][z]));
+                      }
+                  }
+
+              }
+          }
+      }
+  }
+
+  return {
+      'position': new BufferAttribute(new Float32Array(vertices.flat()), 3),
+      'normal': new BufferAttribute(new Float32Array(normals.flat()), 3),
+      'color': new BufferAttribute(new Float32Array(colors.flat()), 3)
+  };
 }
